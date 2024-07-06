4337ce542647e8219b11da7ddf61668580135b46
JennySukut <jennysukut@gmail.com>
2024-07-06 15:00:34 +0200

    Project 15 Edits

--- routes/sign.js
+++ routes/sign.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const { createUser, login } = require("../controllers/users");
+const {
+  validateLoginInformation,
+  validateUserInfo,
+} = require("../middlewares/validation");
 
-router.post("/signin", login);
-router.post("/signup", createUser);
+router.post("/signin", validateLoginInformation, login);
+router.post("/signup", validateUserInfo, createUser);
 
 module.exports = router;
